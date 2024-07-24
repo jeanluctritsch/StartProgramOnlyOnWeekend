43d36c98ae2a6057b41f0e05f3e1f23f21dfb7f3
Jean-Luc Tritsch <bonnepoigne@gmail.com>
2024-07-24 15:36:39 +0200

    Commit initial

---
+++ StartProgramOnWeekend/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+namespace StartProgramOnWeekend;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+            Environment.Exit(1);
+        
+        
+        string path = args[0];
+        string[] arguments = args.Length > 1 ? args[1..] : Array.Empty<string>();
+        
+        if (DateTime.Now.DayOfWeek == DayOfWeek.Saturday || DateTime.Now.DayOfWeek == DayOfWeek.Sunday)
+        {
+            Process.Start(path, arguments);
+        }
+    }
+}
